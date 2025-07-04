d7dea701fc756df6f8378bb4791e0eee9466f181
yCENzh <166907745+yCENzh@users.noreply.github.com>
2025-07-04 14:44:12 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -28,7 +28,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 // https://astro.build/config
 export default defineConfig({
 //	adapter: cloudflare(),
-	site: "https://fuwari.vercel.app/",
+	site: "https://github.com/yCENzh/fuwari/tree/main/src/content/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
