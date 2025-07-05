d0ee470f611c596b1ee668fa3fedba2a65717c2b
yCENzh <166907745+yCENzh@users.noreply.github.com>
2025-07-05 10:30:15 +0800

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,15 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require("tailwindcss/defaultTheme")
 module.exports = {
-  content: [
-    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,mjs}"
-    /*"./components.css"
-    "./src/styles/**/*.css" // 添加这行扫描样式文件*/
-  ],
-  /*corePlugins: {
-    // 禁用预检样式避免冲突
-    preflight: false,
-  },*/
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,mjs}"],
   darkMode: "class", // allows toggling dark mode manually
   theme: {
     extend: {
