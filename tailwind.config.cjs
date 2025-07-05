280d6a2c5462222b007c50dd58391a6717657743
yCENzh <166907745+yCENzh@users.noreply.github.com>
2025-07-05 10:22:50 +0800

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,9 +2,9 @@
 const defaultTheme = require("tailwindcss/defaultTheme")
 module.exports = {
   content: [
-    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,mjs}",
-    "./components.css"
-    "./src/styles/**/*.css" // 添加这行扫描样式文件
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,mjs}"
+    /*"./components.css"
+    "./src/styles/**/*.css" // 添加这行扫描样式文件*/
   ],
   /*corePlugins: {
     // 禁用预检样式避免冲突
