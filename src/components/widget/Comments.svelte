7a857b8bb2bfc644905d011f59825f718aafebf7
yCENzh <166907745+yCENzh@users.noreply.github.com>
2025-07-22 06:45:54 +0800

    Update Comments.svelte

--- src/components/widget/Comments.svelte
+++ src/components/widget/Comments.svelte
@@ -1,5 +1,5 @@
 <section>
-    <script src="https://yCENzh.github.io/Fuwari-Giscus/client.js"
+    <script src="https://ycenzh.github.io/Fuwari-Giscus/client.js"
         data-repo="yCENzh/Fuwari-Giscus"
         data-repo-id="R_kgDOPQMy3A"
         data-category="Announcements"
