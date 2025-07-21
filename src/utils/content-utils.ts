b5f49f6e7230abc00e814ab58e210c60b725a376
yCENzh <166907745+yCENzh@users.noreply.github.com>
2025-07-21 12:56:09 +0800

    Update content-utils.ts

--- src/utils/content-utils.ts
+++ src/utils/content-utils.ts
@@ -92,3 +92,22 @@ export async function getCategoryList(): Promise<Category[]> {
 	}
 	return ret;
 }
+
+export async function getPostSeries(
+  seriesName: string,
+): Promise<{ body: string; data: BlogPostData; slug: string }[]> {
+  const posts = (await getCollection('posts', ({ data }) => {
+    return (
+      (import.meta.env.PROD ? data.draft !== true : true) &&
+      data.series === seriesName
+    )
+  })) as unknown as { body: string; data: BlogPostData; slug: string }[]
+
+  posts.sort((a, b) => {
+    const dateA = new Date(a.data.published)
+    const dateB = new Date(b.data.published)
+    return dateA > dateB ? 1 : -1
+  })
+
+  return posts
+}
