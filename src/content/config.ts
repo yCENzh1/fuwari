413c24cf9865df3f9143d7970196205b1be37343
root <root@admin1.vcn07222308.oraclevcn.com>
2025-08-04 16:01:24 -0700

    fuwari改造笔记

--- src/content/config.ts
+++ src/content/config.ts
@@ -23,6 +23,14 @@ const postsCollection = defineCollection({
 		nextSlug: z.string().default(""),
 	}),
 });
+
+const specCollection = defineCollection({
+	schema: z.object({}),
+});
+
 export const collections = {
 	posts: postsCollection,
+	
+    spec: specCollection,
+    
 };
---
+++ src/content/posts/fuwari.md
@@ -0,0 +1,17 @@
+---
+title: 记一次fuwari改造
+published: 0001-01-01
+draft: true
+---
+
+## 专栏
+
+https://ikamusume7.org/posts/frontend/add_series_field/
+
+## 链接卡片
+
+https://github.com/saicaca/fuwari/pull/324/commits
+
+## 置顶
+
+https://github.com/saicaca/fuwari/pull/317
